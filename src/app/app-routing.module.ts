7f713c214612c6f38e5ca5d18f8fdc6c36106fa7
AndreZigiotti <algazigiotti@gmail.com>
2022-10-08 18:06:06 -0300

    Adicionando rota explore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/explore',
+  },
+  {
+    path: 'explore',
+    loadChildren: () => import('./modules/explore/explore.module').then(m => m.ExploreModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
