6d136c922304a34e29e46ec799d73cd937392c3e
AndreZigiotti <algazigiotti@gmail.com>
2022-10-08 18:05:05 -0300

    Criando módulo explore

---
+++ src/app/modules/explore/explore.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExploreRoutingModule } from './explore-routing.module';
+import { ExploreComponent } from './explore.component';
+import { UiComponentsModule } from "../ui-components/ui-components.module";
+
+
+@NgModule({
+  declarations: [
+    ExploreComponent,
+  ],
+  imports: [
+    CommonModule,
+    ExploreRoutingModule
+  ]
+})
+export class ExploreModule { }
