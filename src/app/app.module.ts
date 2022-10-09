620f18b5cb8234cc0e8887c3a5378c26361f4562
AndreZigiotti <algazigiotti@gmail.com>
2022-10-09 19:04:19 -0300

    Adicionando interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiInterceptor } from "./shared/interceptors/api.interceptor";
 import { UiComponentsModule } from "./modules/ui-components/ui-components.module";
 import { ApiService } from "./shared/services/api.service";
 import { LocalStorageService } from "./shared/services/local-storage.service";
@@ -14,10 +16,16 @@ import { SearchHistoryService } from "./shared/services/search-history.service";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
     UiComponentsModule,
   ],
   providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiInterceptor,
+      multi: true
+    },
     {
       provide: 'IApiService',
       useClass: ApiService
