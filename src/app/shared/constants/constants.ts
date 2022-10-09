42c8fc14424f36225a933400539e5e43e2de8b45
AndreZigiotti <algazigiotti@gmail.com>
2022-10-09 19:03:43 -0300

    Constante de métodos da API

---
+++ src/app/shared/constants/constants.ts
@@ -0,0 +1,10 @@
+export const API_METHODS = {
+  CHART: {
+    GET_TOP_ARTISTS: 'chart.gettopartists',
+    GET_TOP_TAGS: 'chart.gettoptags',
+    GET_TOP_TRACKS: 'chart.gettoptracks'
+  },
+  ARTIST: {
+    SEARCH: 'artist.search'
+  }
+}
